1e3b57b4a165dda43d7f3fff9efbbac450cdcf69
Shomik Ghose <shomik.ghose@antithesis.com>
2024-08-29 16:11:08 -0400

    Provide an absolute path for Gradle builds with make

--- sdk/src/test/java/com/antithesis/sdk/internal/InstrumentationTest.java
+++ sdk/src/test/java/com/antithesis/sdk/internal/InstrumentationTest.java
@@ -1,20 +1,20 @@
 package com.antithesis.sdk.internal;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import com.antithesis.ffi.internal.FfiWrapperJNI;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
 
-import com.antithesis.ffi.internal.FfiWrapperJNI;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class InstrumentationTest {
     @Test
     void testRandom() {
         assumeTrue(FfiWrapperJNI.hasNativeLibrary());
         try {
-             FfiWrapperJNI.loadLibrary();
+            FfiWrapperJNI.loadLibrary();
         } catch (Throwable e) {
             fail("Unable to load native library");
         }
@@ -30,10 +30,10 @@ public class InstrumentationTest {
         jsonNode.put("hello", "world");
 
         try {
-             FfiWrapperJNI.loadLibrary();
+            FfiWrapperJNI.loadLibrary();
         } catch (Throwable e) {
             fail("Unable to load native library");
-        } 
+        }
         String theString = "";
         try {
             theString = mapper.writeValueAsString(jsonNode);
@@ -47,10 +47,10 @@ public class InstrumentationTest {
     void testFlush() {
         assumeTrue(FfiWrapperJNI.hasNativeLibrary());
         try {
-             FfiWrapperJNI.loadLibrary();
+            FfiWrapperJNI.loadLibrary();
         } catch (Throwable e) {
             fail("Unable to load native library");
-        } 
+        }
         FfiWrapperJNI.fuzz_flush();
     }
 }
