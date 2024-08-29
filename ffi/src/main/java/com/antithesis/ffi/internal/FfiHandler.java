76d96df30b3b3d272066ed17f2c25e697ba556af
Shomik Ghose <shomik.ghose@antithesis.com>
2024-08-29 15:31:01 -0400

    Add back some overzealous removals

--- ffi/src/main/java/com/antithesis/ffi/internal/FfiHandler.java
+++ ffi/src/main/java/com/antithesis/ffi/internal/FfiHandler.java
@@ -1,10 +1,10 @@
 package com.antithesis.ffi.internal;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.logging.Logger;
 
 public class FfiHandler implements OutputHandler {
+
+    private static long offset = -1;
 
     public static Optional<OutputHandler> get() {
         try {
@@ -24,6 +24,25 @@ public class FfiHandler implements OutputHandler {
     @Override
     public long random() {
         return FfiWrapperJNI.fuzz_get_random();
+    }
+
+    public long initializeModuleCoverage(long edgeCount, String symbolFilePath) {
+        if (offset != -1) {
+            // A Java application may not contain multiple "modules" in Antithesis terms.
+            throw new IllegalStateException("Antithesis Java instrumentation has already been initialized.");
+        }
+        if (edgeCount > Integer.MAX_VALUE || edgeCount < 1) {
+            throw new IllegalArgumentException("Antithesis Java instrumentation supports [1 ," + Integer.MAX_VALUE + "] edges");
+        }
+        offset = FfiWrapperJNI.init_coverage_module(edgeCount, symbolFilePath);
+        return offset;
+    }
+
+    public void notifyModuleEdge(long edgePlusModule) {
+        // Right now, the Java implementation defers completely to the native library.
+        // See instrumentation.h to understand the logic here. The shim (i.e. StaticModule.java)
+        // is responsible for handling the return value.
+        FfiWrapperJNI.notify_coverage(edgePlusModule);
     }
 
 }
