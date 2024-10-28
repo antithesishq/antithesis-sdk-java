0e5ad1c8f6d5892bb72c28ffb730d3c83c84d0e5
Vlad Korobov <terpimost@gmail.com>
2024-10-28 12:55:09 -0400

    docs: correcting links to antithesis.com/docs/

--- sdk/src/main/java/com/antithesis/sdk/Random.java
+++ sdk/src/main/java/com/antithesis/sdk/Random.java
@@ -9,7 +9,7 @@ import java.util.List;
  * <p>
  * These methods should not be used to seed a conventional PRNG, and should not have their return values stored and used to make a decision at a later time.
  * Doing either of these things makes it much harder for the Antithesis platform to control the history of your program's execution, and also makes it harder for Antithesis to learn which inputs provided at which times are most fruitful.
- * Instead, you should call a method from the random class every time your program or <a href="https://antithesis.com/docs/getting_started/workload.html" target="_blank">workload</a> needs to make a decision, at the moment that you need to make the decision.
+ * Instead, you should call a method from the random class every time your program or <a href="https://antithesis.com/docs/getting_started/first_test/" target="_blank">workload</a> needs to make a decision, at the moment that you need to make the decision.
  * <p>
  * These methods are also safe to call outside the Antithesis environment, where
  * they will fall back on the Java standard class library implementation.
